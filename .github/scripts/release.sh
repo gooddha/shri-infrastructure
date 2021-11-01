4dc1fa41008ae876a96b41b2e1a3e1b5072ffecf
Andrey Adamovich <gooddha@ya.ru>
2021-11-01 14:36:27 +0300

    change event to listen tag push

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 git tag
 
-GIT_VERSION=$(git --version)
+TAG_VERSION=$(git tag);
 
 if [ $? = 0 ]
-then echo $GIT_VERSION
-else 
-    echo "Git не установлен"
-    exit 1
+then echo $TAG_VERSION
 fi

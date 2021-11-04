a27a3e4449abfa8e45340fec9de3aa64fa9e66b1
Andrey Adamovich <gooddha@ya.ru>
2021-11-04 13:42:52 +0300

    add github action url

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo Start release publication
 
+GITHUB_ACTION_URL="$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+echo "GitHub is run action : $GITHUB_ACTIONS_URL"
+
 TAG_VERSION=$(git tag | tail -n 1);
 
 if [ $? = 0 ]

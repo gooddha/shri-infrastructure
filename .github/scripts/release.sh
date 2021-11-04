e767aea1fbc21c07186125194cbc7da7e5cc2123
Andrey Adamovich <gooddha@ya.ru>
2021-11-04 15:56:38 +0300

    add author, changelog echo

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo Start release publication
 
-echo "GitHub is run action : $GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID"
+echo GitHub is run action : $GITHUB_SERVER_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID;
 
 LAST_TAGS=$(git tag | tail -n 2);
-LAST_TAGS2=$(echo $(git tag | tail -n 2));
+LAST_TAG=$(echo $LAST_TAGS | awk '{ print $2 }');
+PREV_TAG=$(echo $LAST_TAGS | awk '{ print $1 }');
 
+echo Last tag version: $LAST_TAG;
+echo Prev tag version: $PREV_TAG;
 
-echo "$LAST_TAGS"
-echo "$LAST_TAGS2"
-# echo $(git log v0.1..v0.2 --oneline)
+CHANGELOG=$(git log --pretty=format:"%s" $PREV_TAG..$LAST_TAG);
+echo $CHANGELOG
 
-# echo $(git tag | tail -n 2)
-
+AUTHOR=$(git show $LAST_TAG | grep Author)
+echo $AUTHOR
 

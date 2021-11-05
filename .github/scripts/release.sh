9ff19c1a04551d865106514654628409b6571be5
Andrey Adamovich <gooddha@ya.ru>
2021-11-05 13:30:27 +0300

    add exit status when script fails

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -9,30 +9,24 @@ PREV_TAG=$(echo $LAST_TAGS | awk '{ print $1 }');
 LAST_TAG=$(echo $LAST_TAGS | awk '{ print $2 }');
 LAST_TAG_DATE=$(git log $LAST_TAG -n 1 | grep Date:);
 
-
 echo Release version: $LAST_TAG;
 echo Release date: $LAST_TAG_DATE;
+AUTHOR=$(git show $LAST_TAG | grep Author);
+echo $AUTHOR
 
 CHANGELOG=$(git log --oneline --decorate $PREV_TAG..$LAST_TAG);
 echo CHANGELOG: $CHANGELOG
-
-AUTHOR=$(git show $LAST_TAG | grep Author);
-echo $AUTHOR
 echo 
 
 echo "Sending request to tracker API for create new task..."
 DESCRIPTION="Release version: $LAST_TAG\n$AUTHOR\n$LAST_TAG_DATE\nRun on: $GITHUB_ACTION\nChangelog:\n$CHANGELOG"
 DESCRIPTION=$(echo "$DESCRIPTION" | sed -z 's/\n/\\n/g');
-
 DATA="{\"summary\": \"Release: $LAST_TAG\", \"queue\": \"TMP\", \"unique\": \"adamovich-$LAST_TAG\", \"description\": \"$DESCRIPTION\"}";
 
 OAUTH="Authorization: OAuth AQAAAAACmEmvAAd5AYEAYatyGkGwgxds0AOn_3M";
 XORG="X-Org-Id: 6461097";
 HOST='https://api.tracker.yandex.net';
-
-
 API_RESPONSE=$(curl -o /dev/null -w "%{http_code}" -s -X 'POST' -H "$OAUTH" -H "$XORG" -H 'Content-Type: application/json' --data "$DATA" $HOST/v2/issues/);
-echo API RESPONSE: $API_RESPONSE;
 
 if [ $API_RESPONSE = 201 ] 
 then echo "Task successfully created";
@@ -48,13 +42,13 @@ then
   if [ -z $TASK_ID ]; then echo "Error, cant get existing task ID"; fi
   if [ -z $TASK_QUEUE_KEY ]; then echo "Error, cant get existing task queue key"; fi
 
-  echo "Existing task URL: https://tracker.yandex.ru/$TASK_QUEUE_KEY"
-  echo "Existing task ID: $TASK_ID"
+  echo "Existing task URL: https://tracker.yandex.ru/$TASK_QUEUE_KEY"  
   echo "Trying to update existing task..."
 
   UPDATE_RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" -X 'PATCH' -H "$OAUTH" -H "$XORG" -H 'Content-Type: application/json' --data "$DATA" $HOST/v2/issues/$TASK_ID);
-  echo $UPDATE_RESPONSE
+  if [ $UPDATE_RESPONSE = 200 ]; then echo "Task is successfully updated!"; else echo "Task updating is fail with error: $UPDATE_RESPONSE"; exit 1; fi;
 
-  if [ $UPDATE_RESPONSE = 200 ]; then echo "Task is successfully updated!"; fi;
-
+else 
+  echo "Task creating is fail with error: $API_RESPONSE";
+  exit 1;
 fi
